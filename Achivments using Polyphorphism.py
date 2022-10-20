1c19444cf89c7c41d0ac56d56ee53354811cf975
Suchir-Phoenix-King <88494177+Suchir-Phoenix-King@users.noreply.github.com>
2022-10-20 17:31:15 +0530

    Add files via upload

---
+++ Achivments using Polyphorphism.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 17:10:57 2022
+
+@author: PC_RC
+"""
+
+class  History_in_space():
+    def  year_2008(self):
+        print("\n", "Events / Achievments in space 2008", "\n")
+        print("India successfully launched Chandrayaan in 2008 in order to explore the moon.The craft was inserted into lunar orbit in its first attempt. One of the greatest achievement of Chandrayaan was the discovery of the water molecules in the lunar soil.")
+        
+    def year_2011(self):
+        print("\n", "Events / Achievments in space 2011", "\n")
+        print("PSLV-C16 successfully launched Three Satellites - RESOURCESAT-2,YOUTHSAT, X-SAT from Sriharikota (April 20, 2011).  Successful launch of GSAT-8 by Ariane-5 VA-202. (May 21, 2011).  PSLV-C17 successfully launched GSAT-12 from Sriharikota (July 15, 2011).")
+        
+        
+class  History_in_medical():
+    def  year_2008(self):
+        print("\n", "Events / Achievments in medical 2008", "\n")
+        print("Although scientists have long discovered how malaria is transferred and know how to prevent it, nearly 1 million people die every year from the disease, according to international estimates.Finally, Dec. 8, the first results of a malaria vaccine that shows promise hit international news.")
+        
+    def year_2011(self):
+        print("\n", "Events / Achievments in medical 2011", "\n")
+        print("The past decade has seen a number of medical headlines involving 3D-organ-printing, up to the point where, recently, researchers managed to create living skin, complete with blood vessels, as well as hearts. While many of these advancements need more research before they can be used in a clinical setting, 3D-printing is set to become more prevalent over the next the next decade, which will make transplanting easier for those in need.")
+        
+        
+class History_in_sports():
+    def year_2008(self):
+        print("\n", "Events / Achievments in sports 2008", "\n")
+        print("In 2008, Abhinav Bindra created history by bagging gold in men's 10m air rifle event at the Beijing Games. His gold remains the country's first and only individual gold medal till date.")
+        
+    def year_2011(self):
+        print("\n", "Events / Achievments in sports 2011", "\n")
+        print("India defeated Pakistan 4-2 in the penalty shoot-out in the final to clinch the inaugural Asian Champions Trophy hockey tournament. In Commonwealth Youth Games held in the Isle of Man India won 3 Gold medals and secured ninth position. Pusarla Venkata Sindhu won a gold for in the women's singles event of badminton.")
+        
+        
+space = History_in_space()
+space.year_2008()
+space.year_2011()
+
+medical = History_in_medical()
+medical.year_2008()
+medical.year_2011()
+
+sports = History_in_sports()
+sports.year_2008()
+sports.year_2011()
